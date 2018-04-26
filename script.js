0bfceac2953bb55aea84179fe02ce74d0278d8e9
Miguel Anthony Ruiz <migz.p.ruiz@gmail.com>
2018-04-25 21:06:41 -0700

    Adding JS Lecture 2

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-//VARIABLES - DATA TYPES
+// LECTURE 1: VARIABLES - DATA TYPES
+/*
 //STRING - text
 var name = "John";
 var lastName = "Smith";
@@ -16,3 +17,27 @@ console.log(name);
 console.log(lastName);
 console.log(age);
 console.log(fullAge);
+*/
+
+// LECTURE 2: VARIABLES 2
+var name = "John";
+var age = 26;
+console.log(name+age);  // John26 - string
+//This is called coercion, JS changes the data type on the fly.
+
+var job, isMarried;
+console.log(job); //Undefined
+job = "Teacher";
+isMarried = false;
+
+console.log(name + " is a " + age + " year old " + job + ". Is he married? " + isMarried);
+
+age = "thirty six"; 
+job = "driver";
+
+console.log(name + " is a " + age + " year old " + job + ". Is he married? " + isMarried);
+
+var lastName = prompt("What is the last name?");
+console.log(lastName);
+
+alert(lastName);
