cf7d38b9dc9f6c9e50d613fda3cf1e3ce9d2a30c
Miguel Anthony Ruiz <migz.p.ruiz@gmail.com>
2018-04-25 20:54:26 -0700

    Adding the practice for variables and data types

---
+++ script.js
@@ -0,0 +1,18 @@
+//VARIABLES - DATA TYPES
+//STRING - text
+var name = "John";
+var lastName = "Smith";
+
+//NUMBER - floats
+var age = 26;
+
+//BOOLEAN - true or false
+var fullAge = true;
+
+//UNDEFINED - no value
+//NULL - non-existent
+
+console.log(name);
+console.log(lastName);
+console.log(age);
+console.log(fullAge);
